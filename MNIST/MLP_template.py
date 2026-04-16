cd90c4e6eb3d5de3525b84c2643958ede257cb91
huunguyen25 <22chrisnguyen@gmail.com>
2026-04-16 01:47:59 -0400

    finished MLP

--- MNIST/MLP_template.py
+++ MNIST/MLP_template.py
@@ -10,7 +10,7 @@ def sigmoid(x):  # manually define the sigmoid
     return 1/(1 + np.exp(-x))
 
 def softmax(x):  # define the softmax 
-    return np.exp(x) / sum(np.exp(x), axis=1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
 
 def dataloader(train_dataset, test_dataset, batch_size=128):
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
@@ -26,7 +26,7 @@ def load_data():
     return dataloader(train_dataset, test_dataset)
 
 class MLP:
-    def __init__(self, input_size, hidden_size, a,lr):  # 
+    def __init__(self, input_size, hidden_size, a, lr):  # 
         
         self.lr = lr
         
@@ -38,21 +38,19 @@ class MLP:
         self.z1 = None
         self.a1 = None
         self.z2 = None
-        self.a2 = None
     
     def forward(self, x):  # forward propagation to get predictions
         # z = W * a + b
         # a = g(z)
-        z1 = (self.Weight1 @ x) + self.bias1
+        z1 = (x @ self.Weight1.T) + self.bias1.T # self.bias1 * 1^T: in lecture multiply by transposed 1 matrix
         a1 = sigmoid(z1)
         
-        z2 = (self.Weight2 @ a1) + self.bias2
+        z2 = (a1 @ self.Weight2.T) + self.bias2.T
         outputs = softmax(z2) 
         
         self.z1 = z1
         self.a1 = a1 
         self.z2 = z2
-        self.a2 = outputs
         
         # 10 by 128 vector
         # rows of class guesses x 128 images per batch
@@ -60,26 +58,27 @@ class MLP:
     
     # pred = output from forward 
     # y = true labels
-    # x = input
+    # x = input (128 by 784 matrix)
     def backward(self, x, y, pred):
-        batch_size = x.shape[1]
+        batch_size = x.shape[0]
         # one-hot encode the labels
 
         # compute the gradients
         
+        # sigmoid + softmax loss function derivation result in y^ - y
         # dZ2 is guess - true class
-        dz2 = self.a2 - y
+        dz2 = pred - y
         #updated weight of second layer
         #dW^(l) = dZ^(l) * A^(l-1)^T
-        dw2 = np.dot(dz2, self.a1.T) / batch_size
+        dw2 = np.dot(dz2.T, self.a1) / batch_size
         #updated bias of second layer
-        db2 = np.dot(dz2 * 1) / batch_size
+        db2 = np.dot(dz2.T, np.ones((batch_size, 1))) / batch_size 
         #dA^(l-1) = W^(l) * dZ^(l)
-        dA1 = np.dot(self.Weight2.T * dz2) / batch_size
-        
-        dz1 = np.dot((self.a1 * (1-self.a1)), dA1)
-        dw1 = np.dot(dz1, x.T) / batch_size
-        db1 = sum(dz1, axis=1, keepdims=True) / batch_size
+        dA1 = np.dot(dz2, self.Weight2)
+
+        dz1 = (self.a1 * (1-self.a1)) * dA1
+        dw1 = np.dot(dz1.T, x) / batch_size
+        db1 = np.dot(dz1.T, np.ones((batch_size, 1))) / batch_size
         
         # update the weights and biases
         self.Weight2 -= self.lr * dw2
@@ -92,9 +91,11 @@ class MLP:
     def train(self, x,y):
         # call forward function
         output = self.forward(x)
-        # calculate loss
-        inner = np.sum(y, np.log(output + 1e-12), axis=1)
-        loss = -np.mean(inner)
+        # calculate loss,
+        # 1e-12 avoid log(0)
+        epsilon = 1e-12
+        output = np.clip(output, epsilon, 1-epsilon)
+        loss = np.mean(-np.sum(y*np.log(output), axis=1))
         # call backward function
         self.backward(x, y, output)
         
@@ -108,21 +109,29 @@ def main():
     input_size = 28*28  # MNIST images are 28x28 pixels
     num_epochs = 100
 
+    hidden_layer_size = 128
+    num_classes = 10
+    learning_rate = 0.001 
+
+    # model object here
+    model = MLP(input_size=input_size, hidden_size=hidden_layer_size, a=num_classes, lr=learning_rate)
 
     # Then, train the model
     for epoch in range(num_epochs):
         total_loss = 0
 
-        for inputs, lables in train_loader:  # define training phase for training model
-            
+        for inputs, labels in train_loader:  # define training phase for training model
+            batch_size = inputs.shape[0]
+            x = inputs.view(batch_size, input_size).numpy()
+            y_labels = labels.numpy()
+            y = np.eye(num_classes)[y_labels]
 
+            loss = model.train(x,y)
+            total_loss += loss
         print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/len(train_loader)}") # print the loss for each epoch
         
-    num_neurons_hidden = 128
-    num_classes = 10
-    learning_rate = 0.001 
-    # declare model object here
-    model = MLP(input_size, num_neurons_hidden, num_classes, learning_rate)
+
+
     
     # Finally, evaluate the model
     correct_pred = 0
